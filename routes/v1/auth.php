dfd0ea70299d3c218612183678df0261af437c60
kamkyi <waihynhtun1994@gmail.com>
2020-11-09 21:53:46 +0630

    Update "

--- routes/v1/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['namespace' => 'Auth','middleware' => ['cors']], function () {
-    Route::post('/login', 'AuthController@login')->name('login');
-    Route::post('/logout', 'AuthController@logout')->name('logout');
-});
